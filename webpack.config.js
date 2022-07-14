cf17752c1f6ba7aa74669d35eefa4ac98e6bf9e5
29td <nonhlanhlamndebele9@gmail.com>
2022-07-14 11:13:52 +0200

    node_modules be gone!

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
-const path = require('path');
-
-module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+import { resolve } from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+export const mode = 'development';
+export const entry = './src/index.js';
+export const devtool = 'inline-source-map';
+export const devServer = {
+  static: './dist',
+};
+export const plugins = [
+  new HtmlWebpackPlugin({
+    title: 'Output Manangement',
+    template: './src/index.html'
+  }),
+];
+export const output = {
+  filename: 'main.js',
+  path: resolve(__dirname, 'dist'),
+  clean: true,
+};
+export const optimization = {
+  runtimeChunk: 'single',
 };
